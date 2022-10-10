d92424608ba86b2c6a759a6eb5a32a40e07fd56f
Dan Fornika <dfornika@gmail.com>
2022-10-09 20:08:49 -0700

    Update

--- src/witchcraft_scratchpad/core.clj
+++ src/witchcraft_scratchpad/core.clj
@@ -6,28 +6,52 @@
 
 
 (comment
-  (wc/fly! me)
   (wc/undo!)
-  (.getSeed (wc/world "world"))
-  (wc/block [0 0 0])
+  
+  (def me (wc/player #_"dfornika"))
+  (wc/fly! me)
+  (wc/fly-speed me)
+  (wc/set-fly-speed me 0.25)
+  (wc/set-health me 20)
 
   (wc/set-time 1000)
   (wc/time)
   (wc/set-game-rule :do-daylight-cycle false)
 
-  (def me (wc/player #_"dfornika"))
+  (wc/add-inventory me :diamond-pickaxe)
+  (wc/add-inventory me :diamond-sword)
+  (wc/add-inventory me :diamond-axe)
+  (wc/add-inventory me :shield)
+  (wc/add-inventory me :diamond-helmet)
+  (wc/add-inventory me :diamond-chestplate)
+  (wc/add-inventory me :diamond-leggings)
+  (wc/add-inventory me :diamond-boots)
+  (wc/add-inventory me :diamond-pickaxe)
+  
+  (wc/add-inventory me :torch)
+  (wc/add-inventory me :redstone-torch)
+  (wc/add-inventory me :stick)
 
+  
   (wc/send-title me (markup/fAnCy "Hello ELLIS!" [:dark-blue :dark-green]))
-  (wc/send-title me "Hello ELLIS!")
+  (wc/send-title me "ELLIS!")
+  (wc/send-title me ":)")
   
   (let [block (wc/get-target-block me)]
     (wc/set-block block (rand-nth (palette/block-materials))))
 
   (let [block (wc/get-target-block me)]
-    (wc/set-block block (rand-nth (palette/block-materials))))
+    (wc/set-block block :redstone-ore))
+  
 
   (let [block (wc/get-target-block me)]
-    (wc/spawn (update (bean (wc/get-target-block me)) :y #(- % 100)) (:enderman wc/entity-types)))
+    (wc/spawn (update (bean (wc/get-target-block me)) :y #(+ % 100)) (:enderman wc/entity-types)))
+
+  
+  (dotimes [n 100]
+    (let [target (select-keys (bean (wc/get-target-block me)) [:x :y :z])]
+      (wc/spawn target (:primed-tnt wc/entity-types))))
+  
 
   
   (wc/loc me)
